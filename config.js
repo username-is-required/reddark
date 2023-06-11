d6594747907f2336b391c6e353a475503ec67022
username-is-required <136007111+username-is-required@users.noreply.github.com>
2023-06-11 15:06:28 +0100

    Create config.js

---
+++ config.js
@@ -0,0 +1,7 @@
+module.exports = {
+    port: process.env.PORT,
+    intervalBetweenRequests: process.env.INTERVAL_BETWEEN_REQUESTS,
+    updateInterval: process.env.UPDATE_INTERVAL,
+    listRefreshInterval: process.env.LIST_REFRESH_INTERVAL,
+    url: process.env.URL
+}
