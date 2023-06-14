eabef81897491aff03ccadcf7177c63068a8deb2
username-is-required <136007111+username-is-required@users.noreply.github.com>
2023-06-14 10:05:35 +0100

    i’m stupid

--- public/index.js
+++ public/index.js
@@ -141,7 +141,7 @@ function updateSubreddit(data, _new = false) {
     
     if (data.status == "private") {
         if (_new && !subsToFilter.includes(data.name.toLowerCase())) {
-            newStatusUpdate("<strong>" + data.name + "</strong>\n" + prevStatus + " → private", "private", function () {
+            newStatusUpdate("<strong>" + data.name + "</strong><br>" + prevStatus + " → private", "private", function () {
                 doScroll(subredditElement);
             })
             audioSystem.playPrivate();
@@ -154,7 +154,7 @@ function updateSubreddit(data, _new = false) {
         }
     } else if (data.status == "restricted") {
         if (_new && !subsToFilter.includes(data.name.toLowerCase())) {
-            newStatusUpdate("<strong>" + data.name + "</strong>\n" + prevStatus + " → restricted", "restricted", function () {
+            newStatusUpdate("<strong>" + data.name + "</strong><br>" + prevStatus + " → restricted", "restricted", function () {
                 doScroll(subredditElement);
             })
             audioSystem.playPrivate();
@@ -167,7 +167,7 @@ function updateSubreddit(data, _new = false) {
         }
     } else {
         if (_new && !subsToFilter.includes(data.name.toLowerCase())) {
-            newStatusUpdate("<strong>" + data.name + "</strong>\n" + prevStatus + " → public", "public", function () {
+            newStatusUpdate("<strong>" + data.name + "</strong><br>" + prevStatus + " → public", "public", function () {
                 doScroll(subredditElement);
             })
             audioSystem.playPublic();
