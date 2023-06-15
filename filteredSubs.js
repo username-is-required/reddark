2e5733cc178c5cf5593fd6e13b7cac19ba61d5fd
username-is-required <136007111+username-is-required@users.noreply.github.com>
2023-06-15 16:19:29 +0100

    create list of continuously filtered subs for backend
    those that are commented out have previously been spamming but are not believed to be currently (but can be refiltered, perhaps permanently, if state change spamming resumes)
    
    list brought over from https://github.com/username-is-required/reddark/blob/3fe78a2b82c27d85f793d72a95fd26fb9d25fb3b/public/index.js#L112-L120, with extras i didnt have there brought over from the main reddark repo: https://github.com/reddark-remix/reddark-remix/blob/2bef755e1e2fdc20bbd711678538b8223d3c6fd1/public/assets/index.js#L10

---
+++ filteredSubs.js
@@ -0,0 +1,11 @@
+module.exports = [
+    // "r/bi_irl",
+    // "r/suddenlybi",
+    // "r/ennnnnnnnnnnnbbbbbby",
+    // "r/seriouslyfuckspez",
+    // "r/feemagers",
+    // "r/brexitatemyface",
+    // "r/emoney",
+    // "r/inzaghi",
+    "r/gtafk",
+];
