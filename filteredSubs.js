5bff69f128a65fa8ca9a399ab403cb8f18555b0b
username-is-required <136007111+username-is-required@users.noreply.github.com>
2023-06-16 01:10:13 +0100

    made something better hopefully

--- filteredSubs.js
+++ filteredSubs.js
@@ -1,8 +1,7 @@
 // a list of filtered subs. all alerts from these subs will be filtered - none will be shown to clients.
 // this is because these subs have been previously known to spam changes to their subreddit's status.
-// ** PLEASE INCLUDE ALL SUB NAMES IN LOWERCASE **
 
-module.exports = [
+var filteredSubs = [
     // "r/bi_irl",
     // "r/suddenlybi",
     // "r/ennnnnnnnnnnnbbbbbby",
@@ -13,3 +12,10 @@ module.exports = [
     // "r/inzaghi",
     "r/gtafk",
 ];
+
+// set them all to lowercase (just in case)
+for (let index in filteredSubs)
+    filteredSubs[index] = filteredSubs[index].toLowerCase();
+
+
+module.exports = filteredSubs;
