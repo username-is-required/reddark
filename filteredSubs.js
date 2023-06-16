60f0963e999ec0abe54cc49bbe98e99416d61693
username-is-required <136007111+username-is-required@users.noreply.github.com>
2023-06-16 01:04:33 +0100

    added some comments to filteredSubs.js

--- filteredSubs.js
+++ filteredSubs.js
@@ -1,3 +1,7 @@
+// a list of filtered subs. all alerts from these subs will be filtered - none will be shown to clients.
+// this is because these subs have been previously known to spam changes to their subreddit's status.
+// ** PLEASE INCLUDE ALL SUB NAMES IN LOWERCASE **
+
 module.exports = [
     // "r/bi_irl",
     // "r/suddenlybi",
